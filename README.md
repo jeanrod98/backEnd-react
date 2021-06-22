ba78934d815c17fc60b5ff2044179b63aef73235
jeanrod98 <jcrod.jc@gmail.com>
2021-06-22 12:54:50 -0500

    servidor backend

---
+++ README.md
@@ -0,0 +1,2 @@
+# backend-react-vinculacion-2021
+Repositorio donde se subirán todos los cambias relacionados al BackEnd de la tienda Online.
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// Crear servidor
+
+const app = express();
+
+//definir puerto
+app.listen(4000, ()=> {
+    console.log('Servidor funcionando 4000')
+    console.log('Esperando respuestas...');
+})
