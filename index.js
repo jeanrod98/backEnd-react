5394407f835ba32a266f4bfd82e8da0234343271
Jose Andres Chavez Leones <joseleones11@gmail.com>
2021-06-22 13:43:23 -0500

    Prueba22

--- index.js
+++ index.js
@@ -7,5 +7,6 @@ const app = express();
 //definir puerto
 app.listen(4000, ()=> {
     console.log('Servidor funcionando 4000')
+    console.log('Esperando respuestas...')
     console.log('Esperando respuestas...');
 })
